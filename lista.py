8a762dc36f59bc94bb8e81d740ea5f2d363645d0
YLL AHMETI <167789279+yllibabaa@users.noreply.github.com>
2025-04-29 17:42:44 +0200

    ...

---
+++ lista.py
@@ -0,0 +1,18 @@
+name_of_list=[ "item1","item2","item3","item4","item5"]
+shopping_list=["banane",3.5,3]
+print(shopping_list[0])
+
+name_of_list.append("item5")
+name_of_list.count()
+lista2=name_of_list.copy()
+list3=lista2
+list2[0]="alma"
+
+print("lista 3",list3)
+print("lista 2",list2)
+
+lista2.insert(1,"item10")
+print("lista 2",list2)
+lista2.pop[1]
+lista2.remove("item4")
+lista2.reverse()
